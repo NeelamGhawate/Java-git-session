23376cb1ec5ff386796eebf1ba38c4a3b957997e
neelam <nilam.ghawate@gmail.com>
2020-09-17 18:43:46 +0530

    comminting test case

---
+++ SciCalciTest.java
@@ -0,0 +1,10 @@
+public class SciCalciTest
+{
+	public static void main(string args[])
+	{
+		Run|Debug
+		SciCalci c=new SciCalci();
+		double result=c.cos(34);
+		System.out.println("The result is %d", result");
+	}
+} 
