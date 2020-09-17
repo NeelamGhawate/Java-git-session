87ede727ae31188fefb99c22cc9d55083a6d99ce
neelam <nilam.ghawate@gmail.com>
2020-09-17 18:28:13 +0530

    comminting SciClient file

---
+++ SciCalci.java
@@ -0,0 +1,19 @@
+public class SciCalci
+{
+	private static Math math;
+	
+	public double tan(double a)
+	{
+		return Math.tan(a);
+	}
+	
+	public double sin(double a)
+	{
+		return Math.sin(a);
+	}
+	
+	public double cos(double a)
+	{
+		return Math.cos(a);
+	}
+} 
